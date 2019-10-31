1e8261a42157bd3014655d9fa9b6b4d30e6d2749
Mattkvls <matthew.katsivelis@gmail.com>
2019-10-31 23:30:55 +0200

    create the Search Class data model and add getResults method also update the index so we can save results and query to search object

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,8 @@
 // Global app controller
-import axios from 'axios';
-//API Key: e364306a650d0fd91e1e19ce9fb08aa6
-//Query https://www.food2fork.com/api/search
-//recipe requests : https://www.food2fork.com/api/get
+import Search from './models/Search'
+import { getPriority } from 'os';
 
-async function getResults(query){
-    
-    try{
-        //instead of using fetch which is not supported by all browsers
-    //we install axios npm package
-    const proxy=`https://cors-anywhere.herokuapp.com/`; //if there is a need for proxy prepend a ${proxy} to the axios link
-    const key=`e364306a650d0fd91e1e19ce9fb08aa6`;
-    const res= await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes=res.recipes;
-   console.log(res);
-    //will return a promise
-    }catch(error){
-        alert(error);
-    }
-}
-getResults('pizza');
+const search= new Search('pizza'); //we create an object for one Search
+console.log(search);
+search.getResults();
+//both the query and results are saved in the search object
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,7 +1,26 @@
-// build our data model with Classes 
-//we descibe the data model for the search. The data is the query, search results 
+import axios from 'axios';
+//API Key: e364306a650d0fd91e1e19ce9fb08aa6
+//Query https://www.food2fork.com/api/search
+//recipe requests : https://www.food2fork.com/api/get
+
+//describe the data model for the search with Classes
+//the data is query and search results
 export default class Search{
     constructor(query){
         this.query=query;
     }
+    //method that gets results for the search query
+    async getResults(){
+        try{
+            const proxy=`https://cors-anywhere.herokuapp.com/`; //if there is a need for proxy prepend a ${proxy} to the axios link
+            const key=`e364306a650d0fd91e1e19ce9fb08aa6`;
+            const res= await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+
+            //save the recipes to the object
+            this.result=res.data.recipes;
+        }catch(error){
+            alert(error);
+        }
+    }
+
 }
