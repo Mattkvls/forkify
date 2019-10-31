63440aa6a9d663c1bbc6110c3ba0a1c1f593b9cc
Mattkvls <matthew.katsivelis@gmail.com>
2019-10-31 18:58:41 +0200

    update the .gitignore file with the recap.js

---
+++ src/js/models/Search.js
@@ -0,0 +1,7 @@
+// build our data model with Classes 
+//we descibe the data model for the search. The data is the query, search results 
+export default class Search{
+    constructor(query){
+        this.query=query;
+    }
+}
