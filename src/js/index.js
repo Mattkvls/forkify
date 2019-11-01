e3ed0e67b0f0a3d63b750fb9c2341c8ba136b42f
Mattkvls <matthew.katsivelis@gmail.com>
2019-11-01 13:31:05 +0200

    build the Search Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,40 @@
 // Global app controller
-import Search from './models/Search'
-import { getPriority } from 'os';
+import Search from './models/Search';
 
-const search= new Search('pizza'); //we create an object for one Search
-console.log(search);
-search.getResults();
-//both the query and results are saved in the search object
+/*Global state of the app
+-Search object 
+-current recipe object 
+- shopping list object
+-liked recipes
+*/
+const state={}; //each time we reload the app the state will be empty
+
+//create a function to put the logic and call it inside the event listener
+const controlSearch= async ()=>{
+    //1)get query from the view 
+    const query='pizza'; //we hardcode it for now ->make a function at views to get it 
+
+    if(query){
+        //2) create new search object and add it to state 
+        state.search= new Search(query);
+
+        // 3) prepeare UI for results 
+
+        //4) search for recipes 
+       await state.search.getResults();//returns a promise
+
+        // 5) render results in UI (THIS HAPPENS AFTER WE RECEIVE RESULTS FROM API )
+        console.log(state.search.result);
+    }
+}
+
+//select and listen for submits from the form 
+document.querySelector('.search').addEventListener('submit',e=>{
+//stop the form reloads when submits
+e.preventDefault();
+controlSearch();
+})
+
+
+
+
